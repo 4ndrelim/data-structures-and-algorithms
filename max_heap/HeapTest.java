53795a9b8b7f5069854044ba95a2c1ab4a1d333c
andre <tiltedaf3@gmail.com>
2022-07-13 00:37:22 +0800

    Shift heap files to heap folder

---
+++ max_heap/HeapTest.java
@@ -0,0 +1,28 @@
+/**
+ * Basic testing of Heap structure
+ */
+public class HeapTest {
+  public static void main(String[] args) {
+    Heap<Integer> heap = new Heap<>();
+    heap.offer(1);
+		heap.offer(2);
+		heap.offer(9);
+		heap.offer(4);
+		heap.offer(5);
+    System.out.println(heap)
+
+		System.out.println("Peek: " + heap.peek());
+		System.out.println("Poll: " + heap.poll());
+		System.out.println("Peek: " + heap.peek());
+
+		heap.offer(6);
+
+		System.out.println("Peek: " + heap.peek());
+		System.out.println("Poll: " + heap.poll());
+		System.out.println("Poll: " + heap.poll());
+    System.out.println(heap);
+
+    heap.heapify(new ArrayList<>(Arrays.asList(5,4,6,7,2,1,9,8,0,3)));
+    System.out.println(heap);
+  }
+}
