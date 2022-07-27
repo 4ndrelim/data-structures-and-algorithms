a3a013b90c4cb0c0d77cd9fe4b94cec6362fd595
andre <tiltedaf3@gmail.com>
2022-07-27 21:39:15 +0800

    Add implementation for stack

---
+++ stack_and_queue/stack.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+/**
+ * Simple implementation of a stack using ArrayList.
+ * Note: ArrayList can be implemented using LinkedList
+ *       and hence the code below can be replicated with
+ *       LinkedList.
+ * Callable methods:
+ *  push()
+ *  pop()
+ *  peek()
+ *  empty()
+ * Note: Calling pop() or peek() on an empty stack returns null
+ * @param <T> generic type of objects to be stored in the stack
+ */
+public class Stack<T> {
+  List<T> stack;
+
+  /**
+   * Constructor to create an empty stack
+   */
+  public Stack() {
+    stack = new ArrayList<T>();
+  } 
+
+  /**
+   * Constructor to create a stack with a given list holding the same type
+   */
+  public Stack(List<T> lst) {
+    stack = lst;
+  }
+
+  /**
+   * Constructor to create a stack given a sequence of objects of the same generic type
+   */
+  public Stack(T ...lst) {
+    stack = new ArrayList<T>();
+    for (T obj : lst) {
+      stack.add(obj);
+    }
+  }
+
+  /**
+   * @param item element to be pushed as the last element into the stack
+   */
+  public void push(T item) {
+    stack.add(item);
+  }
+
+  /**
+   * Removes the last element from the stack.
+   * @return last element of the stack; null if the stack is empty
+   */
+  public T pop() {
+    if (this.empty()) {
+      return null;
+    }
+    // note that the operation below is O(1) when called on the last index
+    return stack.remove(stack.size() - 1);
+  }
+
+  /**
+   * Displays the last element of the stack.
+   * @return last element of the stack; null if the stack is empty
+   */
+  public T peek() {
+    if (this.empty()) {
+      return null;
+    }
+    // similarly, below is an O(1) operation
+    return stack.get(stack.size() - 1);
+  }
+
+  /**
+   * Checks if the stack is empty.
+   * @return boolean value representing whether the stack is empty
+   */
+  public boolean empty() {
+    return stack.size() == 0;
+  }
+} 
