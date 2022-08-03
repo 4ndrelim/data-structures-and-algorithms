f340fd4bfb7fa1ddc3ce6da04b4196cb9125ace6
andre <tiltedaf3@gmail.com>
2022-08-04 02:19:53 +0800

    Add docs

--- linked-list/LinkedList.java
+++ linked-list/LinkedList.java
@@ -1,5 +1,32 @@
 import java.util.*;
 
+/**
+ * There are many variations when it comes to implementing linked lists. Here's mine.
+ * A linked list that tracks both head and tail may improve the complexity of some of the methods below
+ * (e.g being able to branch off and create a new Linked List in O(1) rather than iterating to find size of branched off list)
+ * 
+ * Constructors:
+ * LinkedList()                      -- Initialise a link list with a null head; size = 0
+ * LinkedList(Node<T> head)          -- given a node, make this node the head of a new linked list; iterate to find the size
+ * LinkedList(Node<T>head, int size) -- given a head node and size of linked list specified; 
+ *                                      made private to avoid client from constructing a linked list with invalid size
+ * 
+ * Callable methods are:
+ * size()                    -- Gets the size of the linked list
+ * insertFront(T object)     -- inserts the object at the front of the linked list
+ * insertEnd(T object)       -- inserts the object at the end of the linked list
+ * insert(T object, int idx) -- inserts the object at the specified index of the linked list
+ * remove(int idx)           -- remove the node at the specified index
+ * delete(T object)          -- delete the 1st encounter of the specified object from the linked list
+ * pop()                     -- remove the last node from the linked list
+ * poll()                    -- remove the first node from the linked list
+ * search(T object)          -- search for the 1st encounter of the node that holds the specified object
+ * get(int idx)              -- get the node at the specified index
+ * reverse()                 -- reverse the linked list (head of linked list now starts from the back)
+ * sort()                    -- sorts the linked list by their natural order
+ * 
+ * @param <T> generic type for objects to be stored in the linked list
+ */
 public class LinkedList<T extends Comparable<T>> {
   private Node<T> head;
   private int size;
@@ -25,18 +52,38 @@ public class LinkedList<T extends Comparable<T>> {
     this.size = size;
   }
 
+  /**
+   * Gets the size of the linked list.
+   * @return int value
+   */
   public int size() {
     return this.size;
   }
 
+  /**
+   * Inserts the object at the front of the linked list
+   * @param object to be inserted
+   * @return boolean representing whether insertion was successful
+   */
   public boolean insertFront(T object) {
     return insert(object, 0);
   }
 
+  /**
+   * Inserts the object at the end of the linked list
+   * @param object to be inserted 
+   * @return boolean representing whether insertion was successful
+   */
   public boolean insertEnd(T object) {
     return insert(object, this.size);
   }
 
+  /**
+   * inserts the object at the specified index of the linked list
+   * @param object to be inserted
+   * @param idx index which the object is to be inserted into
+   * @return boolean representing whether insertion was successful
+   */
   public boolean insert(T object, int idx) {
     if (idx > size) {
       System.out.println("Index out of bounds.");
@@ -69,6 +116,11 @@ public class LinkedList<T extends Comparable<T>> {
     return true;
   }
 
+  /**
+   * remove the node at the specified index
+   * @param idx of the node to be removed
+   * @return node's value
+   */
   public T remove(int idx) {
     if (idx >= size) {
       System.out.println("Index out of bounds.");
@@ -92,6 +144,11 @@ public class LinkedList<T extends Comparable<T>> {
     return trav.val;
   }
 
+  /**
+   * delete the 1st encounter of the specified object from the linked list
+   * @param object to search and delete
+   * @return boolean whether the delete op was successful
+   */
   public boolean delete(T object) {
     int idx = search(object);
     if (idx != -1) {
@@ -101,14 +158,27 @@ public class LinkedList<T extends Comparable<T>> {
     return false;
   }
 
+  /**
+   * remove the last node from the linked list
+   * @return value of popped node
+   */
   public T pop() {
     return remove(size() - 1);
   }
 
+  /**
+   * remove the first node from the linked list
+   * @return value of polled node
+   */
   public T poll() {
     return remove(0);
   }
 
+  /**
+   * search for the 1st encounter of the node that holds the specified object
+   * @param object
+   * @return index of the node found
+   */
   public int search(T object) {
     Node<T> trav = head;
     int idx = 0;
@@ -127,6 +197,11 @@ public class LinkedList<T extends Comparable<T>> {
     return -1;
   }
 
+  /**
+   * get the node at the specified index
+   * @param idx of node to be found
+   * @return node found
+   */
   public Node<T> get(int idx) {
     Node<T> trav = head;
     if (idx < this.size && trav != null) {
@@ -138,6 +213,9 @@ public class LinkedList<T extends Comparable<T>> {
     return null;
   }
 
+  /**
+   * reverse the linked list
+   */
   public void reverse() {
     if (head == null || head.next == null) {
       return;
@@ -157,6 +235,9 @@ public class LinkedList<T extends Comparable<T>> {
     head = newHead;
   }
 
+  /**
+   * sorts the linked list by their natural order
+   */
   public void sort() {
     if (this.size <= 1) {
       return;
@@ -173,6 +254,12 @@ public class LinkedList<T extends Comparable<T>> {
     head = merge(this, next);
   }
 
+  /**
+   * Merge routine helper function for two sorted linked lists
+   * @param first sorted linked list
+   * @param second sorted linked list
+   * @return head of merged linked list
+   */
   private Node<T> merge(LinkedList<T> first, LinkedList<T> second) {
     Node<T> headFirst = first.head;
     Node<T> headSecond = second.head;
@@ -213,6 +300,9 @@ public class LinkedList<T extends Comparable<T>> {
     return ret.substring(0, ret.length());
   }
   
+  /**
+   * Node class for linked list
+   */
   private static class Node<T> {
     T val;
     Node<T> next;
