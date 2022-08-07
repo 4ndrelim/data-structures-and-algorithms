f8e45a892f74e49a7f13c8cb00b73290ae5bacb1
andre <tiltedaf3@gmail.com>
2022-08-07 17:40:11 +0800

    Add doc for monotonic queue

--- stack_and_queue/monotonic_queue/MonotonicQueue.java
+++ stack_and_queue/monotonic_queue/MonotonicQueue.java
@@ -2,13 +2,32 @@ import java.util.*;
 
 /**
  * Implementation of a non-increasing monotonic queue.
+ * Callable methods are:
+ * isEmpty()
+ * max()
+ * pop()
+ * push()
+ * @param <T> generic type for objects to be stored or queried   
  */
 class MonotonicQueue<T extends Comparable<T>> {
   private Deque<Pair<T>> dq = new ArrayDeque<>(); // or LinkedList
 
+  /**
+   * Checks if queue is empty.
+   * @return boolean
+   */
   public boolean isEmpty() {
     return dq.isEmpty();
   }
+
+  /**
+   * Push an object into the queue, maintaining the non-increasing property.
+   * Note that the object will be wrapped in a node and prior nodes whose 
+   * objects' values are less the object to be inserted will be popped from the queue.
+   * A count of the number of removed queues will be stored in the current node to track 
+   * number of replaced nodes.
+   * @param T obj to be inserted
+   */
   public void push(T obj) {
     Integer count = 0;
     while (!dq.isEmpty() && obj.compareTo(dq.peekLast().value) > 0) {
@@ -18,6 +37,10 @@ class MonotonicQueue<T extends Comparable<T>> {
     dq.offerLast(new Pair<T>(obj, count));
   }
 
+  /**
+   * Returns the highest value stored in the queue.
+   * @return the first value of the queue.
+   */
   public T max() {
     if (isEmpty()) {
       return null;
@@ -25,6 +48,12 @@ class MonotonicQueue<T extends Comparable<T>> {
     return dq.peek().value;
   }
 
+  /**
+   * Removes the node with the highest value in the queue; i.e the first node;
+   * but if the tracked deletion count is greater than 0; i.e there have been nodes
+   * which have been replaced, then the current node will not yet be removed and 
+   * its value will be returned until the queue is done processing all the replaced nodes.
+   */
   public void pop() {
     if (dq.peek().countDeleted > 0) {
       dq.peek().countDeleted -= 1;
@@ -34,6 +63,10 @@ class MonotonicQueue<T extends Comparable<T>> {
     return;
   }
 
+  /**
+   * Node class that is represented as a pair.
+   * Tracks the deleted count and the value to be wrapped.
+   */
   private static class Pair<T> {
     private T value;
     private Integer countDeleted;
