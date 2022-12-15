a1d37d2b65e9ddd930612f56ee8532113c468cbb
andre <tiltedaf3@gmail.com>
2022-12-15 14:31:41 +0800

    Add structure for segment tree

---
+++ segment_tree/test.java
@@ -0,0 +1,11 @@
+/**
+ * Basic testing
+ */
+
+ public class test {
+    public static void main(String[] args) {
+        int[] t = new int[] {1,2,3,4,5,6,7};
+        SegmentTree tree = new SegmentTree(t);
+        System.out.println(tree.sumRange(2, 5));
+    }
+ }
