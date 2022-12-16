4fcc55d900741376016c4779a673cafff0e10332
andre <tiltedaf3@gmail.com>
2022-12-16 17:47:54 +0800

    Specify methods to implement

---
+++ segment_tree/improved_segment_tree/ImprovedSegmentTree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+/**
+ * Improved segment tree that does not require O(2^n) space complexity
+ * Uses TreeNode to store relevant information for range sum queries
+ */
+
+ public class ImprovedSegmentTree {
+    public ImprovedSegmentTree(int[] nums) {
+
+    }
+
+    public void update(int idx, int val) {
+
+    }
+
+    public int sumRange(int left, int right) {
+        
+    }
+ }
