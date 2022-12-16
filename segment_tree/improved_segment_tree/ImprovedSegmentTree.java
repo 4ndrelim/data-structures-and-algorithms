1fb659c6f2dac844664b315f75faf83894f20b0c
andre <tiltedaf3@gmail.com>
2022-12-16 20:53:27 +0800

    Complete build tree method

--- segment_tree/improved_segment_tree/ImprovedSegmentTree.java
+++ segment_tree/improved_segment_tree/ImprovedSegmentTree.java
@@ -5,8 +5,25 @@ import java.util.*;
  */
 
  public class ImprovedSegmentTree {
-    public ImprovedSegmentTree(int[] nums) {
+    TreeNode tree;
 
+    private TreeNode buildTree(int[] nums, int start, int end) {
+        TreeNode curr = new TreeNode(start, end);
+        if (start == end) {
+            curr.sum = nums[start];
+        } else {
+            int m = start + (end-start)/2;
+            TreeNode left = buildTree(nums, start, m);
+            TreeNode right = buildTree(nums, m+1, end);
+            curr.left = left;
+            curr.right = right;
+            curr.sum = left.sum + right.sum;
+        }
+        return curr;
+    }
+
+    public ImprovedSegmentTree(int[] nums) {
+        this.tree = buildTree(nums, 0, nums.length-1);
     }
 
     public void update(int idx, int val) {
@@ -14,6 +31,6 @@ import java.util.*;
     }
 
     public int sumRange(int left, int right) {
-        
+        return 0;
     }
  }
