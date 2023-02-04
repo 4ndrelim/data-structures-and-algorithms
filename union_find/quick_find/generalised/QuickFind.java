77dde7c7119c8498a397781462ccd62bf7c074b2
4ndrelim <andre_lin@u.nus.edu>
2023-02-04 15:26:42 +0800

    Implement simple quick find

---
+++ union_find/quick_find/generalised/QuickFind.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation of quick-find algorithm; Turns a list of objects into a dats structure that supports union operations
+ * @param <T> generic type of object to be stored
+ */
+public class QuickFind<T> {
+    private List<T> list;
+
+    public QuickFind() {
+        this.list = new ArrayList<T>();
+    }
+
+    public QuickFind(List<T> input) {
+        this.list = input;
+    }
+
+    public add()
+}
