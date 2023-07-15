59e3461dbdc2cf730643c98c1d39e8354ebd08ce
4ndrelim <tiltedaf3@gmail.com>
2023-07-15 18:17:02 +0800

    Complete KMP implementation

---
+++ out/production/src/src/algorithms/patternFinding/README.md
@@ -0,0 +1,23 @@
+# Knuth-Moris-Pratt Algorithm
+
+KMP match is a type of pattern-searching algorithm.
+Pattern-searching problems is prevalent across many fields of CS, for instance,
+in text editors when searching for a pattern, in computational biology sequence matching problems,
+in NLP problems, and even for looking for file patterns for effective file management.
+It is hence crucial that we develop an efficient algorithm.
+
+Naively, we can look for patterns in a given sequence in O(nk) where n is the length of the sequence and k
+is the length of the pattern. We do this by iterating every character of the sequence, and look at the 
+immediate k-1 characters that come after it. This is not a big issue if k is known to be small, but there's
+no guarantee this is the case.
+
+KMP does this in O(n+k) by making use of previously identified sub-patterns. It identifies sub-patterns
+by first processing the pattern input in O(k) time, allowing identification of patterns in
+O(n) traversal of the sequence. More details found in the src code.
+
+![KMP](../../../assets/kmp.png)
+Image Source: GeeksforGeeks
+
+
+If you have trouble understanding the implementation, 
+here is a good [video](https://www.youtube.com/watch?v=EL4ZbRF587g). 
--- src/algorithms/patternFinding/KMP.java
+++ src/algorithms/patternFinding/KMP.java
@@ -4,35 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * O(nk) -- > O(n+k)
- * This is still IP.
+ * Implementation of KMP.
+ *
+ * Illustration of getPrefixIndices:
+ * Here we make a distinction between position and index. The position is basically 1-indexed.
+ * Note the return indices are still 0-indexed of the pattern string.
+ *  Position:  0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
+ *   Pattern:      A   B   C   A   B   C   N   O   A   B   C   A   B   C   A ...
+ *    Return: -1   0   0   0   1   2   3   0   0   1   2   3   4   5   6   4 ...
+ *             ^  an indexing trick; consider 1-indexed characters for clarity and simplicity in the main algor
+ *                 ^ 'A' is the first character of the pattern string,
+ *                 there is no prefix ending before its index, 0, that can be matched with
+ *                     ^   ^ 'B' and 'C' cannot be matched with any prefix which are just 'A' and 'AB'
+ *                             ^ Can be matched with an earlier 'A'. So we store 1. Which can also be interpreted as the
+ *                             index of the next character to match against!
+ *                                 ^   ^ Similarly, continue matching
+ *                                          ^  ^ No matches, so 0
+ *                                                 ^   ^   ^   ^   ^   ^ Match with prefix until index 6!
+ *                                                                         ^ where the magic happens, we know ABC of
+ *                                                                         index 0-2 exists and can 'restart' from there
+ *
  */
 public class KMP {
+    /**
+     * Find and indicate all suffix that match with a prefix.
+     * @param pattern to search
+     * @return an array of indices where the suffix ending at each position of they array can be matched with
+     * corresponding a prefix of the pattern ending before the specified index
+     */
     private static int[] getPrefixIndices(String pattern) {
         int len = pattern.length();
         int[] prefixIndices = new int[len + 1];
         prefixIndices[0] = -1;
 
         int currPrefixMatched = 0; // num of chars of prefix pattern currently matched
-        int i = 1; // iterate until the end
-        while (i < len) {
-            if (pattern.charAt(i) == pattern.charAt(currPrefixMatched)) {
+        int pos = 2; // Starting from the 2nd character, recall 1-indexed
+        while (pos <= len) {
+            if (pattern.charAt(pos-1) == pattern.charAt(currPrefixMatched)) {
                 currPrefixMatched += 1;
-                i += 1;
                 // note, the line below can also be interpreted as the index of the next char to match
-                prefixIndices[i] = currPrefixMatched; // an indexing trick, store at the ith pos, num of chars matched
+                prefixIndices[pos] = currPrefixMatched; // an indexing trick, store at the pos, num of chars matched
+                pos += 1;
             } else if (currPrefixMatched > 0) {
                 // go back to a previous known match and try to match again
                 currPrefixMatched = prefixIndices[currPrefixMatched];
             } else {
                 // unable to match, time to move on
-                i += 1;
-                prefixIndices[1] = 0; // recall prefixIndices is 1-indexed and hence update after incrementing
+                prefixIndices[pos] = 0;
+                pos += 1;
             }
         }
         return prefixIndices;
     }
 
+    /**
+     * Main logic of KMP. Iterate the sequence, looking for patterns. If a difference is found, resume matching from
+     * a previously identified sub-pattern, if possible.
+     * @param sequence to search against
+     * @param pattern to search for
+     * @return start indices of all occurrences of pattern found
+     */
     public static List<Integer> findOccurrences(String sequence, String pattern) {
         int sLen = sequence.length();
         int pLen = pattern.length();
