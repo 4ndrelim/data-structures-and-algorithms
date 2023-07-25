79af08ae503c9329d6560d2bfcd86b59f572e192
4ndrelim <tiltedaf3@gmail.com>
2023-07-25 21:23:03 +0800

    Add scripts for counting sort and kmp

---
+++ scripts/algorithms/patternFinding/RunKMP.java
@@ -0,0 +1,50 @@
+import src.algorithms.patternFinding.KMP;
+
+import java.util.List;
+
+public class RunKMP {
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+//////////////////////////////////////////   This section is for user input   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    static String seq = "abclaabcabcabc";
+    static String pattern = "abc";
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+    public static void main(String[] args) {
+        List<Integer> indices = KMP.findOccurrences(seq, pattern);
+        display(indices);
+        prettyPrint(seq, pattern, indices);
+    }
+
+    public static void display(List<Integer> arr) {
+        StringBuilder toDisplay = new StringBuilder("[");
+        for (int num : arr) {
+            toDisplay.append(String.format("%d ", num));
+        }
+        toDisplay = toDisplay.replace(toDisplay.length()-1, toDisplay.length(), "]");
+        System.out.println(toDisplay);
+    }
+
+    public static void prettyPrint(String seq, String pattern, List<Integer> indices) {
+        StringBuilder prettySeq = new StringBuilder(" Sequence: ");
+        StringBuilder prettyPtr = new StringBuilder("Start Pts: ");
+        int currPos = 0;
+        for (int i = 0; i < seq.length(); i++) {
+            char curr = seq.charAt(i);
+            prettySeq.append(String.format("%c  ", curr));
+
+            if (currPos < indices.size()) {
+                int currIdx = indices.get(currPos);
+                if (currIdx == i) {
+                    prettyPtr.append("^  ");
+                    currPos++;
+                } else {
+                    prettyPtr.append("   ");
+                }
+            }
+        }
+        System.out.println("  Pattern: " + pattern);
+        System.out.println(prettySeq);
+        System.out.println(prettyPtr);
+    }
+}
