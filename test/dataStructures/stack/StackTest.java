6801baae011571ff097bce79ed1a9448efc6948f
yeoshuheng <yeoshuheng@gmail.com>
2023-08-04 20:05:14 +0800

    fix: unit test order

--- test/dataStructures/stack/StackTest.java
+++ test/dataStructures/stack/StackTest.java
@@ -7,26 +7,26 @@ public class StackTest {
     @Test
     public void testEmpty(){
         Stack<Integer> stk = new Stack<>();
-        Assert.assertEquals(stk.pop(), null);
-        Assert.assertEquals(stk.peek(), null);
+        Assert.assertEquals(null, stk.pop());
+        Assert.assertEquals(null, stk.peek());
     }
 
     @Test
     public void testPopAndPeek() {
         Stack<Integer> stk = new Stack<>(1, 2, 3);
-        Assert.assertEquals(stk.peek().toString(), "3");
-        Assert.assertEquals(stk.pop().toString(), "3");
-        Assert.assertEquals(stk.peek().toString(), "2");
+        Assert.assertEquals("3", stk.peek().toString());
+        Assert.assertEquals("3", stk.pop().toString());
+        Assert.assertEquals("2", stk.peek().toString());
     }
 
     @Test
     public void testPush() {
         Stack<Integer> stk = new Stack<>();
         stk.push(1);
-        Assert.assertEquals(stk.peek().toString(), "1");
+        Assert.assertEquals("1", stk.peek().toString());
         stk.push(2);
         stk.push(3);
-        Assert.assertEquals(stk.peek().toString(), "3");
+        Assert.assertEquals("3", stk.peek().toString());
     }
 
 }
