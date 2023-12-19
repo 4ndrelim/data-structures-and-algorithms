d4d03310e85abafbd8151a5aabfb060206f6836e
junneng <soojunneng01@gmail.com>
2023-12-19 22:39:17 +0800

    Add test cases

--- src/test/java/dataStructures/binarySearchTree/BinarySearchTreeTest.java
+++ src/test/java/dataStructures/binarySearchTree/BinarySearchTreeTest.java
@@ -2,27 +2,82 @@ package dataStructures.binarySearchTree;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BinarySearchTreeTest {
     @Test
     public void insert_shouldCorrectlyInsertNodes() {
-        // Create BST instance
         BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
-
-        // Insert elements
         bst.insert(5, "Five");
         bst.insert(3, "Three");
         bst.insert(7, "Seven");
         bst.insert(2, "Two");
         bst.insert(4, "Four");
 
-        // Expected in-order traversal result
-        List<Integer> expected = Arrays.asList(2, 3, 4, 5, 7);
-
         // Perform in-order traversal and get result
-        List<Integer> result = bst.inOrderTraversal();
+        List<String> result = bst.getInorder();
 
-        // Assert that the result matches the expected list
+        // Expected in-order traversal result
+        List<String> expected = Arrays.asList("Key: 2, Value: Two", "Key: 3, Value: Three", "Key: 4, Value: Four", "Key: 5, Value: Five", "Key: 7, Value: Seven");
+
         assert result.equals(expected);
     }
 
+    @Test
+    public void delete_shouldCorrectlyHandleLeafNode() {
+        BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+        bst.insert(5, "Five");
+        bst.insert(3, "Three");
+        bst.insert(7, "Seven");
+
+        bst.delete(7);
+
+        // Perform in-order traversal and get result
+        List<String> result = bst.getInorder();
+
+        // Expected in-order traversal result after deletion
+        List<String> expected = Arrays.asList("Key: 3, Value: Three", "Key: 5, Value: Five");
+
+        assert result.equals(expected);
+    }
+
+    @Test
+    public void delete_shouldCorrectlyHandleNodeWithOneChild() {
+        BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+        bst.insert(5, "Five");
+        bst.insert(3, "Three");
+        bst.insert(6, "Six");
+        bst.insert(7, "Seven");
+
+        bst.delete(6);
+
+        // Perform in-order traversal and get result
+        List<String> result = bst.getInorder();
+
+        // Expected in-order traversal result after deletion
+        List<String> expected = Arrays.asList("Key: 3, Value: Three", "Key: 5, Value: Five", "Key: 7, Value: Seven");
+
+        assert result.equals(expected);
+    }
+
+    @Test
+    public void delete_shouldCorrectlyHandleNodeWithTwoChildren() {
+        BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+        bst.insert(5, "Five");
+        bst.insert(3, "Three");
+        bst.insert(7, "Seven");
+        bst.insert(2, "Two");
+        bst.insert(4, "Four");
+
+        bst.delete(3);
+
+        // Perform in-order traversal and get result
+        List<String> result = bst.getInorder();
+
+        // Expected in-order traversal result after deletion
+        List<String> expected = Arrays.asList("Key: 2, Value: Two", "Key: 4, Value: Four", "Key: 5, Value: Five", "Key: 7, Value: Seven");
+
+        assert result.equals(expected);
+    }
 }
