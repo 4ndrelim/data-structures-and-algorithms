3159ffa349a2789e638b871ff1112f6edcdd378a
yeoshuheng <yeoshuheng@gmail.com>
2024-02-08 22:49:11 +0800

    feat: Add Delete for RB

--- src/main/java/dataStructures/rbTree/RBTree.java
+++ src/main/java/dataStructures/rbTree/RBTree.java
@@ -27,10 +27,19 @@ public class RBTree<T extends Comparable<T>> {
     }
 
     /**
+     * Getter for root.
+     * @return The root of the RB tree.
+     */
+    public RBNode<T> getRoot() {
+        return this.root;
+    }
+
+    /**
      * Inserts element into tree.
      * @param element The element to insert.
+     * @return The newly added node.
      */
-    public void insert(T element) {
+    public RBNode<T> insert(T element) {
         RBNode<T> toAdd = new RBNode<>(element);
         RBNode<T> prev = nil;
         RBNode<T> curr = root;
@@ -51,11 +60,12 @@ public class RBTree<T extends Comparable<T>> {
             prev.setRight(toAdd);
         }
         this.fixInsert(toAdd);
+        return toAdd;
     }
 
     /**
-     * Fixes tree upwards upon insert operation.
-     * @param node Node to fix upwards from.
+     * Fixes red-black properties upwards upon insert operation.
+     * @param node The node to fix.
      */
     public void fixInsert(RBNode<T> node) {
         while (node.getParent().getColor().equals(RBNode.VAL.RED)) {
@@ -95,6 +105,28 @@ public class RBTree<T extends Comparable<T>> {
             }
         }
         root.setColor(RBNode.VAL.BLACK);
+    }
+
+    /**
+     * Find element. In this case, this is "it-exists" checker.
+     * However, in a map-based RB Tree like Java's TreeMap, this would conduct a search based on
+     * a key and return the value in the key : value pair.
+     * @param element
+     * @return The element if it's in the tree, else null.
+     */
+    public T get(T element) {
+        RBNode<T> curr = root;
+        while (curr != nil) {
+            if (curr.getElement().equals(element)) {
+                return element;
+            }
+            if (element.compareTo(curr.getElement()) < 0) {
+                curr = curr.getLeft();
+            } else {
+                curr = curr.getRight();
+            }
+        }
+        return null;
     }
 
     /**
@@ -139,6 +171,130 @@ public class RBTree<T extends Comparable<T>> {
         }
         temp.setRight(node);
         node.setParent(temp);
+    }
+
+    /**
+     * Helper function that transposes node A into B.
+     * @param a The node to transpose.
+     * @param b The node to transpose to.
+     */
+    public void transplant(RBNode<T> a, RBNode<T> b) {
+        if (b.getParent() == nil) {
+            root = a;
+        } else if (b == b.getParent().getLeft()) {
+            b.getParent().setLeft(a);
+        } else {
+            b.getParent().setRight(a);
+        }
+        a.setParent(b.getParent());
+    }
+
+    /**
+     * Gets the node with the minimum value.
+     * @param node The node tree is rooted at.
+     * @return The node with the minimum value.
+     */
+    public RBNode<T> getMin(RBNode<T> node) {
+        while (node != nil) {
+            node = node.getLeft();
+        }
+        return node;
+    }
+
+    /**
+     * Deletes a node from the tree.
+     * @param node The node to delete.
+     */
+    public void delete(RBNode<T> node) {
+        RBNode<T> x;
+        RBNode<T> y;
+        RBNode.VAL deletedColor = node.getColor();
+        if (node.getLeft() == nil) {
+            x = node.getRight();
+            this.transplant(node.getRight(), node);
+        } else if (node.getRight() == nil) {
+            x = node.getLeft();
+            this.transplant(node.getLeft(), node);
+        } else {
+            y = this.getMin(node);
+            deletedColor = y.getColor();
+            x = y.getRight();
+            if (y.getParent() == node) {
+                x.setParent(y);
+            } else {
+                this.transplant(y.getRight(), y);
+                y.setRight(node.getRight());
+                y.getRight().setParent(y);
+            }
+            this.transplant(y, node);
+            y.setLeft(node.getLeft());
+            y.getLeft().setParent(y);
+            y.setColor(node.getColor());
+        }
+        if (deletedColor.equals(RBNode.VAL.BLACK)) {
+            this.fixDelete(x);
+        }
+    }
+
+    /**
+     * Fixes red-black properties after delete operation.
+     * @param node The node to fix.
+     */
+    private void fixDelete(RBNode<T> node) {
+        while (node != root && node.getColor().equals(RBNode.VAL.BLACK)) {
+            if (node == node.getParent().getLeft()) {
+                RBNode<T> brother = node.getParent().getRight();
+                if (brother.getColor().equals(RBNode.VAL.RED)) {
+                    brother.setColor(RBNode.VAL.BLACK);
+                    node.getParent().setColor(RBNode.VAL.RED);
+                    this.leftRotate(node.getParent());
+                    brother = node.getParent().getRight();
+                }
+                if (brother.getLeft().getColor().equals(RBNode.VAL.BLACK)
+                    && brother.getRight().getColor().equals(RBNode.VAL.BLACK)) {
+                    brother.setColor(RBNode.VAL.RED);
+                    node = node.getParent();
+                } else {
+                    if (brother.getRight().getColor().equals(RBNode.VAL.BLACK)) {
+                        brother.getLeft().setColor(RBNode.VAL.BLACK);
+                        brother.setColor(RBNode.VAL.RED);
+                        this.rightRotate(brother);
+                        brother = node.getParent().getRight();
+                    }
+                    brother.setColor(node.getParent().getColor());
+                    node.getParent().setColor(RBNode.VAL.BLACK);
+                    brother.getRight().setColor(RBNode.VAL.BLACK);
+                    this.leftRotate(node.getParent());
+                    node = this.root;
+                }
+            } else {
+                RBNode<T> brother = node.getParent().getLeft();
+                if (brother.getColor().equals(RBNode.VAL.RED)) {
+                    brother.setColor(RBNode.VAL.BLACK);
+                    node.getParent().setColor(RBNode.VAL.RED);
+                    this.rightRotate(node.getParent());
+                    brother = node.getParent().getLeft();
+                }
+                if (brother.getLeft().getColor().equals(RBNode.VAL.BLACK)
+                    && brother.getRight().getColor().equals(RBNode.VAL.BLACK)) {
+                    brother.setColor(RBNode.VAL.RED);
+                    node = node.getParent();
+                } else {
+                    if (brother.getLeft().getColor().equals(RBNode.VAL.BLACK)) {
+                        brother.getRight().setColor(RBNode.VAL.BLACK);
+                        brother.setColor(RBNode.VAL.RED);
+                        this.leftRotate(brother);
+                        brother = node.getParent().getLeft();
+                    }
+                    brother.setColor(node.getParent().getColor());
+                    node.getParent().setColor(RBNode.VAL.BLACK);
+                    brother.getLeft().setColor(RBNode.VAL.BLACK);
+                    this.rightRotate(node.getParent());
+                    node = this.root;
+                }
+            }
+            node.setColor(RBNode.VAL.BLACK);
+        }
     }
 
 }
