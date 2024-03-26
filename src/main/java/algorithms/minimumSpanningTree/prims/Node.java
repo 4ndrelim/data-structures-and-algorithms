7f842085605ccdaac701736cd67d48a661c79915
junnengsoo <soojunneng01@gmail.com>
2024-03-26 22:42:32 +0800

    Add adjacency matrix and new test cases

--- src/main/java/algorithms/minimumSpanningTree/prims/Node.java
+++ src/main/java/algorithms/minimumSpanningTree/prims/Node.java
@@ -2,18 +2,28 @@ package algorithms.minimumSpanningTree.prims;
 
 import java.util.Objects;
 
+/**
+ * Node class to represent a node in the graph
+ */
 public class Node {
     private boolean isVisited = false;
     private int currMinWeight = Integer.MAX_VALUE; // Current minimum weight to get to this node
     private int index; // Index of this node in the adjacency matrix
-    private String identifier;
+    private final String identifier;
 
+    /**
+     * Constructor
+     * @param identifier
+     * @param index
+     */
     public Node(String identifier, int index) {
         this.identifier = identifier;
         this.index = index;
     }
 
-    // Getter and setter for currMinWeight
+    /**
+     * Getter and setter for currMinWeight
+     */
     public int getCurrMinWeight() {
         return currMinWeight;
     }
@@ -22,21 +32,26 @@ public class Node {
         this.currMinWeight = currMinWeight;
     }
 
-    // Getter and setter for isVisited
+    /**
+     * Getter for identifier
+     * @return identifier
+     */
     public boolean isVisited() {
         return isVisited;
     }
 
+    /**
+     * Setter for isVisited
+     * @param isVisited
+     */
     public void setVisited(boolean isVisited) {
         this.isVisited = isVisited;
     }
 
-    // Getter for identifier
-    public String getIdentifier() {
-        return identifier;
-    }
-
-    // Getter and setter for index
+    /**
+     * Getter for identifier
+     * @return identifier
+     */
     public int getIndex() {
         return index;
     }
@@ -45,23 +60,8 @@ public class Node {
         this.index = index;
     }
 
-    // toString method
     @Override
     public String toString() {
         return "Node{" + "identifier='" + identifier + '\'' + ", index=" + index + '}';
-    }
-
-    // equals and hashCode based on identifier and index
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node node = (Node) o;
-        return index == node.index && Objects.equals(identifier, node.identifier);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(identifier, index);
     }
 }
