73b815cadb461779b5ca34b88bcbcdea88281f1e
Mohammad Sufian <91954762+Suf42@users.noreply.github.com>
2024-08-14 11:37:40 +0800

    Update LRU.java
    Update name of public class

--- src/main/java/dataStructures/lruCache/LRU.java
+++ src/main/java/dataStructures/lruCache/LRU.java
@@ -2,28 +2,28 @@ package dataStructures.lruCache;
 
 import java.util.HashMap;
 
-public class doublyLinkedList {
+public class LRU { // doublyLinkedList
     int key;
     int val;
-    doublyLinkedList next;
-    doublyLinkedList prev;
+    LRU next;
+    LRU prev;
 }
 
 class LRUCache {
-    doublyLinkedList dllHead;
-    doublyLinkedList dllTail;
-    HashMap<Integer, doublyLinkedList> keyToNode = new HashMap();
+    LRU dllHead;
+    LRU dllTail;
+    HashMap<Integer, LRU> keyToNode = new HashMap();
     int capacity;
     int lengthOfList = 0;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
 
-        dllHead = new doublyLinkedList();
+        dllHead = new LRU();
         dllHead.key = -1;
         dllHead.val = -1;
 
-        dllTail = new doublyLinkedList();
+        dllTail = new LRU();
         dllTail.key = -1;
         dllTail.val = -1;
 
@@ -36,7 +36,7 @@ class LRUCache {
     public int get(int key) {
         if(!keyToNode.containsKey(key)) return -1;
 
-        doublyLinkedList temp = keyToNode.get(key);
+        LRU temp = keyToNode.get(key);
         temp.prev.next = temp.next;
         temp.next.prev = temp.prev;
 
@@ -51,10 +51,10 @@ class LRUCache {
     public void put(int key, int value) {
         boolean addingNewNode = true;
 
-        doublyLinkedList newlyCached;
+        LRU newlyCached;
 
         if(!keyToNode.containsKey(key)) {
-            newlyCached = new doublyLinkedList();
+            newlyCached = new LRU();
             newlyCached.key = key;
             newlyCached.val = value;
             keyToNode.put(key, newlyCached);
